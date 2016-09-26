91c60113499f382b3c7c3b4a5b3d9c6f65b86367
atehnix <atehnix@gmail.com>
2016-09-26 10:32:08 +0300

    Fix use Schema

--- publish/database/2016_01_01_100000_create_vk_requester_tables.php
+++ publish/database/2016_01_01_100000_create_vk_requester_tables.php
@@ -1,6 +1,7 @@
 <?php
 
 use ATehnix\LaravelVkRequester\Models\VkRequest;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
